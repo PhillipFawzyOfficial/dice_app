f3e53be537e04a316e7e62708ff212d28be26b73
phillip <flipking037@gmail.com>
2026-03-07 21:50:37 +0200

    finsh HomeViewBody

--- lib/feature/home/presentation/views/widgets/home_view_body.dart
+++ lib/feature/home/presentation/views/widgets/home_view_body.dart
@@ -5,6 +5,15 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(constext) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.indigo],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(child: Image.asset('assets/images/dice-1.png', width: 200)),
+    );
   }
 }
